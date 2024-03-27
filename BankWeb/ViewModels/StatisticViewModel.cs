6e47d7e7bb0e10da7c8a13be9f2dc95c6c23b23c
acerd44 <maulvids@gmail.com>
2024-03-27 21:25:54 +0100

    .

--- BankWeb/ViewModels/StatisticViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BankWeb.ViewModels
-{
-    public class StatisticViewModel
-    {
-        public int Customers { get; set; }
-        public int Accounts { get; set; }
-        public decimal BalanceSum { get; set; }
-    }
-}
